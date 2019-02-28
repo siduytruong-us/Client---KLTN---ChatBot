d7eca6dfb6f7044e860d2be9c9c9a0ae4e5e0921
Cris <duytruong0702@gmail.com>
2019-02-28 13:30:58 +0700

    Create index layout

--- src/App.jsx
+++ src/App.jsx
@@ -1,18 +1,54 @@
 import React, { Component } from 'react';
 import {BrowserRouter, Switch, Route} from 'react-router-dom'
 import index from './Components/index'
-
+import { Widget, addResponseMessage  } from 'react-chat-widget';
+import socketIOClient  from "socket.io-client"
+import Header from "./Components/Navigation/Header"
+import Footer from "./Components/Navigation/Footer"
 
 class App extends Component {
+  
+  constructor(props) {
+    super(props);
+    this.state = { 
+        socket:null,
+    }
+    this.socket = null
+  }
+
+  componentDidMount() {
+    addResponseMessage("Welcome to this awesome chat!");
+  }
+
+  componentWillMount() { 
+      this.socket = socketIOClient("http://localhost:3001")
+      this.socket.on("chat message", msg => {
+          addResponseMessage(msg)
+      });
+  }
+
+  handleNewUserMessage = (newMessage) => {
+      this.socket.emit("chat message", newMessage)
+  }
+
+
+
   render() {
     return (
       <BrowserRouter >
       <div className = "App">
-
+        <Header/>
           <Switch>        
               <Route exact path = "/" component = {index}/>
              
           </Switch>
+          <Widget 
+            handleNewUserMessage={this.handleNewUserMessage}
+            profileAvatar= "https://cdn.shopifycloud.com/hatchful-web/assets/2adcef6c1f7ab8a256ebdeba7fceb19f.png"
+            titleAvatar = "https://cdn.shopifycloud.com/hatchful-web/assets/2adcef6c1f7ab8a256ebdeba7fceb19f.png"
+          />
+
+        <Footer/>
       </div>
       </BrowserRouter>
      
--- src/Components/index.jsx
+++ src/Components/index.jsx
@@ -1,45 +1,137 @@
 import React, { Component } from 'react';
-import { Widget, addResponseMessage  } from 'react-chat-widget';
-import socketIOClient  from "socket.io-client"
-import { Spin } from 'antd';
-
-
 
 class index extends Component {
-    constructor(props) {
-        super(props);
-        this.state = { 
-            socket:null,
-        }
-        this.socket = null
-    }
-
-    componentDidMount() {
-        addResponseMessage("Welcome to this awesome chat!");
-      }
-
-    componentWillMount() { 
-        this.socket = socketIOClient("http://localhost:3001")
-        this.socket.on("chat message", msg => {
-            addResponseMessage(msg)
-        });
-    }
-    
-    handleNewUserMessage = (newMessage) => {
-        this.socket.emit("chat message", newMessage)
-    }
-
     render() {
         return (
             <div>
-                <br/>
-                {/* <Spin className = "antd-spinner"> */}
-                <Widget 
-                    handleNewUserMessage={this.handleNewUserMessage}
-                    profileAvatar= "https://cdn.shopifycloud.com/hatchful-web/assets/2adcef6c1f7ab8a256ebdeba7fceb19f.png"
-                    titleAvatar = "https://cdn.shopifycloud.com/hatchful-web/assets/2adcef6c1f7ab8a256ebdeba7fceb19f.png"
-                />
-               {/* </Spin> */}
+                <section id="mu-slider">
+                    <div class="mu-slider-single">
+                    <div class="mu-slider-img">
+                        <figure>
+                        <img src="/assets/img/slider/1.jpg" alt="img"/>
+                        </figure>
+                    </div>
+                    <div class="mu-slider-content">
+                        <h4>Welcome To Varsity</h4>
+                        <span></span>
+                        <h2>We Will Help You To Learn</h2>
+                        <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Dolor amet error eius reiciendis eum sint unde eveniet deserunt est debitis corporis temporibus recusandae accusamus.</p>
+                        <a href="#" class="mu-read-more-btn">Read More</a>
+                    </div>
+                    </div>
+                    <div class="mu-slider-single">
+                    <div class="mu-slider-img">
+                        <figure>
+                        <img src="/assets/img/slider/2.jpg" alt="img"/>
+                        </figure>
+                    </div>
+                    <div class="mu-slider-content">
+                        <h4>Premiumu Quality Free Template</h4>
+                        <span></span>
+                        <h2>Best Education Template Ever</h2>
+                        <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Dolor amet error eius reiciendis eum sint unde eveniet deserunt est debitis corporis temporibus recusandae accusamus.</p>
+                        <a href="#" class="mu-read-more-btn">Read More</a>
+                    </div>
+                    </div>
+                    <div class="mu-slider-single">
+                    <div class="mu-slider-img">
+                        <figure>
+                        <img src="/assets/img/slider/3.jpg" alt="img"/>
+                        </figure>
+                    </div>
+                    <div class="mu-slider-content">
+                        <h4>Exclusivly For Education</h4>
+                        <span></span>
+                        <h2>Education For Everyone</h2>
+                        <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Dolor amet error eius reiciendis eum sint unde eveniet deserunt est debitis corporis temporibus recusandae accusamus.</p>
+                        <a href="#" class="mu-read-more-btn">Read More</a>
+                    </div>
+                    </div>
+                </section>          
+            
+                <section id="mu-about-us">
+                    <div class="container">
+                    <div class="row">
+                        <div class="col-md-12">
+                        <div class="mu-about-us-area">           
+                            <div class="row">
+                            <div class="col-lg-6 col-md-6">
+                                <div class="mu-about-us-left">
+                                <div class="mu-title">
+                                    <h2>About Us</h2>              
+                                </div>
+                                <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Rerum pariatur fuga eveniet soluta aspernatur rem, nam voluptatibus voluptate voluptates sapiente, inventore. Voluptatem, maiores esse molestiae.</p>
+                                <ul>
+                                    <li>Lorem ipsum dolor sit amet, consectetur adipisicing elit.</li>
+                                    <li>Saepe a minima quod iste libero rerum dicta!</li>
+                                    <li>Voluptas obcaecati, iste porro fugit soluta consequuntur. Veritatis?</li>
+                                    <li>Ipsam deserunt numquam ad error rem unde, omnis.</li>
+                                    <li>Repellat assumenda adipisci pariatur ipsam eos similique, explicabo.</li>
+                                </ul>
+                                <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Perspiciatis quaerat harum facilis excepturi et? Mollitia!</p>
+                                </div>
+                            </div>
+                            <div class="col-lg-6 col-md-6">
+                                <div class="mu-about-us-right">                            
+                                <a id="mu-abtus-video" href="https://www.youtube.com/embed/9Z49DscgnJY" target="mutube-video">
+                                <img src="https://edu2review.com/upload/article-images/2018/07/7933/1920x1080_dai-hoc-khoa-hoc-tu-nhien.jpg" alt="img"/>
+                                </a>                
+                                </div>
+                            </div>
+                            </div>
+                        </div>
+                        </div>
+                    </div>
+                    </div>
+                </section>
+            
+                <section id="mu-abtus-counter">
+                    <div class="container">
+                    <div class="row">
+                        <div class="col-md-12">
+                        <div class="mu-abtus-counter-area">
+                            <div class="row">
+                            <div class="col-lg-3 col-md-3 col-sm-3">
+                                <div class="mu-abtus-counter-single">
+                                <span class="fa fa-book"></span>
+                                <h4 class="counter">568</h4>
+                                <p>Subjects</p>
+                                </div>
+                            </div>
+                            <div class="col-lg-3 col-md-3 col-sm-3">
+                                <div class="mu-abtus-counter-single">
+                                <span class="fa fa-users"></span>
+                                <h4 class="counter">3500</h4>
+                                <p>Students</p>
+                                </div>
+                            </div>
+                            <div class="col-lg-3 col-md-3 col-sm-3">
+                                <div class="mu-abtus-counter-single">
+                                <span class="fa fa-flask"></span>
+                                <h4 class="counter">65</h4>
+                                <p>Modern Lab</p>
+                                </div>
+                            </div>
+                            <div class="col-lg-3 col-md-3 col-sm-3">
+                                <div class="mu-abtus-counter-single no-border">
+                                <span class="fa fa-user-secret"></span>
+                                <h4 class="counter">250</h4>
+                                <p>Teachers</p>
+                                </div>
+                            </div>
+                            </div>
+                        </div>
+                        </div>
+                    </div>
+                    </div>
+                </section>
+            
+            
+            
+            
+            
+            
+            
             </div>
         );
     }
