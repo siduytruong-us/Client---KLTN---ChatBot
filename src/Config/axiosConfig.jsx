be553cc3f92e2f86ec283e0641ae4464ee7022e2
Cris <duytruong0702@gmail.com>
2019-04-20 14:54:17 +0700

    update axios config

--- src/Config/axiosConfig.jsx
+++ src/Config/axiosConfig.jsx
@@ -3,7 +3,9 @@ import Axios from "axios";
 
 const axios = Axios
 
-const domain = "http://localhost:3001"
+// const domain = "http://localhost:3001"
+const domain = "https://server-college-handbook.herokuapp.com"
+
 axios.defaults.baseURL = domain
 // axios.defaults.headers.post['Content-Type'] ='application/x-www-form-urlencoded';
 // axios.defaults.headers.post['Access-Control-Allow-Origin'] = 'http://localhost:3002';
